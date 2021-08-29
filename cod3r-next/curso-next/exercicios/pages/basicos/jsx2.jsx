62007482c14141bf5ef58f9ea2619a5f705a4094
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-29 18:02:19 -0300

    CTD

--- cod3r-next/curso-next/exercicios/pages/basicos/jsx2.jsx
+++ cod3r-next/curso-next/exercicios/pages/basicos/jsx2.jsx
@@ -1,8 +1,10 @@
 export default function jsx2() {
+  const array = [<li>Paulo</li>, <li>Ana</li>, <li>Pedro</li>];
+
   const conteudo = (
     <div>
       <h2>Opa</h2>
     </div>
   );
-  return conteudo;
+  return array;
 }
---
+++ cod3r-next/curso-next/exercicios/pages/basicos/jsx3.jsx
@@ -0,0 +1,7 @@
+export default function jsx3() {
+  return (
+    <div>
+      <h1>Opa</h1>
+    </div>
+  );
+}
