429d1b2d0c99528909d6f8b7a4af3bf05470d668
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-29 15:32:03 -0300

    CTD

---
+++ cod3r-next/Typescript/tipos/tipos.js
@@ -0,0 +1,19 @@
+"use strict";
+//string
+var nome = 'João';
+console.log(nome);
+// numbers
+var idade = 20;
+console.log(idade);
+//boolean
+var bacana = true;
+// tipos explicitos
+var minhaIdade;
+minhaIdade = 30;
+//array
+var hobbies = [];
+hobbies.push("ler");
+hobbies.push("dormir");
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [10];
---
+++ cod3r-next/Typescript/tipos/tipos.ts
@@ -0,0 +1,33 @@
+//string
+
+let nome:string = 'João'
+
+console.log(nome)
+
+// numbers
+
+let idade:number = 20;
+console.log (idade)
+
+//boolean
+
+let bacana:boolean = true
+
+
+// tipos explicitos
+
+let minhaIdade: number
+
+minhaIdade = 30
+
+//array
+
+let hobbies: any[] = []
+
+hobbies.push("ler")
+hobbies.push("dormir")
+
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies = [10]
