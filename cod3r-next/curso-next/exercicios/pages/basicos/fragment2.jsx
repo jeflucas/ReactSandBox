652ae27f084ad32073e5a7d6e9864729a84b6781
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-30 21:37:07 -0300

    CTD

--- cod3r-next/curso-next/exercicios/pages/basicos/fragment2.jsx
+++ cod3r-next/curso-next/exercicios/pages/basicos/fragment2.jsx
@@ -1,8 +1,9 @@
 export default function fragment() {
-    return(
-        <h1>ok</h1>
-        <h2>opa</h2>
-        <h3>eae</h3>
-    )
-    
+  return (
+    <>
+      <h1>ok</h1>
+      <h2>opa</h2>
+      <h3>eae</h3>
+    </>
+  );
 }
