e0588bfaf6dfe590130808a210ab6eb6df38dab9
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-31 21:21:55 -0300

    CTD

--- cod3r-next/curso-next/exercicios/pages/css/integracao1.jsx
+++ cod3r-next/curso-next/exercicios/pages/css/integracao1.jsx
@@ -1,9 +1,12 @@
+//Não posso importar aqui
+//o import foi feito em app.js
+
 export default function integracao() {
   return (
-    <>
+    <div className="integracao1">
       <div className="vermelha">Texto #01</div>
       <div className="azul">Texto #02</div>
       <div className="branca">Texto #03</div>
-    </>
+    </div>
   );
 }
