527ff58e41be121d4d8402719183b7f660d7575b
jeflucas <jef.lucas@hotmail.co.uk>
2021-09-04 20:27:46 -0300

    CTD

--- cod3r-next/curso-next/exercicios/pages/render/repeticao1.jsx
+++ cod3r-next/curso-next/exercicios/pages/render/repeticao1.jsx
@@ -1,9 +1,23 @@
 export default function repeticao1() {
-  return (
-    <ul>
-      <li>Elemento 01</li>
-      <li>Elemento 02</li>
-      <li>Elemento 03</li>
-    </ul>
-  );
+  const listaAprovados = [
+    "fulano1",
+    "fulano2",
+    "fulano3",
+    "fulano4",
+    "fulano5",
+    "fulano6",
+    "fulano7",
+  ];
+
+  function renderizarLista() {
+    const itens = [];
+
+    for (let index = 0; index < listaAprovados.length; index++) {
+      itens.push(<li key={index}>{listaAprovados[index]}</li>);
+    }
+
+    return itens;
+  }
+
+  return <ul>{renderizarLista()}</ul>;
 }
