e69594fc6d50d59cd406fcb4d6f2422895452ef3
jeflucas <jef.lucas@hotmail.co.uk>
2021-09-04 18:26:18 -0300

    CTD

--- cod3r-next/curso-next/tabuleiro/pages/index.js
+++ cod3r-next/curso-next/tabuleiro/pages/index.js
@@ -1,10 +1,10 @@
+import EstiloTabuleiro from "../components/Subdivisao";
+
 export default function Home() {
   return (
-    <div style={{ backgroundColor: "#fff" }}>
+    <div style={{ backgroundColor: "#333" }}>
       <h1>Tabuleiro</h1>
-      <div
-        style={{ height: "100px", width: "100px", backgroundColor: "#000" }}
-      ></div>
+      <div style={{ height: "100px", width: "100px" }}></div>
     </div>
   );
 }
