3393440c181e2fe74b2418296e13251ab652790e
jeflucas <jef.lucas@hotmail.co.uk>
2021-09-12 13:36:13 -0300

    CTD

--- cod3r-next/curso-next/exercicios/pages/render/condicional2.jsx
+++ cod3r-next/curso-next/exercicios/pages/render/condicional2.jsx
@@ -1,11 +1,14 @@
 import If from "../../components/if";
 
 export default function condicional2() {
-  const num = 2;
+  const num = 5;
   return (
     <div>
       <If teste={num % 2 == 0}>
         <h2>{num} é par</h2>
+      </If>
+      <If teste={num % 2 != 0}>
+        <h2>{num} é impar</h2>
       </If>
     </div>
   );
