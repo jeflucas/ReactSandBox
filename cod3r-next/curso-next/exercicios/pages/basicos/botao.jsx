0166600b7f3713b0412be4eaaeeb3194231a8f1c
jeflucas <jef.lucas@hotmail.co.uk>
2021-09-14 10:46:18 -0300

    CTD

--- cod3r-next/curso-next/exercicios/pages/basicos/botao.jsx
+++ cod3r-next/curso-next/exercicios/pages/basicos/botao.jsx
@@ -3,9 +3,33 @@ function acao1() {
 }
 
 export default function botao() {
+  function acao2() {
+    console.log("Ação 02");
+  }
+
+  function acao5(e) {
+    console.log(e);
+  }
+
   return (
     <div>
-      <button onClick={acao1()}>Botão 01</button>
+      <button onClick={acao1}>Botão 01</button>
+      <button onClick={acao2}>Botão 02</button>
+      <button
+        onClick={function () {
+          console.log("Ação 03");
+        }}
+      >
+        Botão 03
+      </button>
+      <button
+        onClick={() => {
+          console.log("Ação 04");
+        }}
+      >
+        Botão 04
+      </button>
+      <button onClick={acao5}>Botão 05</button>
     </div>
   );
 }
